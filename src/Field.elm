105845abfe6ccd45f8f440c415cbc082bd71bb9e
Jonathan Fishbein <jonathanfishbein@protonmail.com>
2019-09-07 11:09:20 -0400

    Convert first parameter of power to Float

--- src/Field.elm
+++ src/Field.elm
@@ -26,7 +26,7 @@ type alias Field a =
     , subtract : a -> a -> a
     , multiply : a -> a -> a
     , divide : a -> a -> a
-    , power : Int -> a -> a
+    , power : Float -> a -> a
     }
 
 
@@ -40,5 +40,5 @@ realField =
     , subtract = (-)
     , multiply = (*)
     , divide = (/)
-    , power = \exponent base -> base ^ toFloat exponent
+    , power = \exponent base -> base ^ exponent
     }
